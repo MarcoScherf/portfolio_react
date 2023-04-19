da289582719608c57394e350f3af8d523bc0b8a3
MarcoScherf <scheco13@gmail.com>
2023-04-19 19:41:45 +0200

    navigation added and styled

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.scss';
+import 'aos/dist/aos.css';
+
+import Navigation from './components/navigation/navigation';
+
+function App() {
+  return (
+    <div >
+     <Navigation />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/navigation/navigation.jsx
@@ -0,0 +1,113 @@
+import React, { Component } from "react";
+import AOS from "aos";
+import "./style.scss";
+
+import { withTranslation } from "react-i18next";
+import i18n from "../../i18n";
+
+class Navigation extends Component {
+  componentDidMount() {
+    AOS.init();
+  }
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: false,
+      isLanguageEN: false,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleChangeLanguage = (lng) => {
+    if (lng === "de") {
+      this.setState({ isLanguageEN: true });
+    } else {
+      this.setState({ isLanguageEN: false });
+    }
+    i18n.changeLanguage(lng);
+  };
+
+  handleClick() {
+    this.setState({ isOpen: !this.state.isOpen });
+  }
+
+  state = {};
+  render() {
+    const { t } = this.props;
+    return (
+      <>
+        <header data-aos="fade-down" data-aos-delay="300">
+          <div className="container">
+            <a href="/">
+              <span className="logo">[M.S]</span>
+            </a>
+            <div className="right">
+              <div className="language">
+                <span
+                  onClick={() => this.handleChangeLanguage("en")}
+                  className={this.state.isLanguageEN ? "" : "active"}
+                >
+                  EN
+                </span>
+                |
+                <span
+                  onClick={() => this.handleChangeLanguage("de")}
+                  className={this.state.isLanguageEN ? "active" : ""}
+                >
+                  DE
+                </span>
+              </div>
+              <div onClick={this.handleClick}>
+                <div id="menu" className={this.state.isOpen ? "open" : ""}>
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </header>
+        <div className={this.state.isOpen ? "nav menu-open" : "nav"}>
+          <nav>
+            <div className="nav-link">{t("nav.about")}</div>
+            <div className="nav-link">{t("nav.skills")}</div>
+            <div className="nav-link">{t("nav.portfolio")}</div>
+            <div className="nav-link">{t("nav.contact")}</div>
+          </nav>
+          <div className="footer">
+            <div className="footer-container">
+              <div className="left">
+                <span className="greeting">{t("nav.hello")}</span>
+                <span className="mail">mail@marco-scherf.de</span>
+              </div>
+              <div className="social ">
+                <a
+                  href="https://github.com/MarcoScherf"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img src="assets/img/github.svg" alt="github" />
+                </a>
+                <a href="/">
+                  <img src="./assets/img/mail.svg" alt="email" />
+                </a>
+                <a
+                  href="https://www.linkedin.com/in/marco-scherf-107956249"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img src="./assets/img/in.svg" alt="linkedin" />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
+}
+
+export default withTranslation()(Navigation);
